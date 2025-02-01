68c2a67c51fceb71c2e989a96b8e2ae36ba9ea46
prititi <pt446914@gmail.com>
2025-02-02 01:48:30 +0530

    change _id to id

--- model/product.model.js
+++ model/product.model.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    category: String,
-    stockQuantity: Number,
-  }, { collection: 'products' });
-  
-  const Product = mongoose.model("Product", ProductSchema);
-  
+  name: String,
+  price: Number,
+  category: String,
+  stockQuantity: Number,
+}, { collection: 'products' });
+
+ProductSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id;  // Rename `_id` to `id`
+    delete ret._id;     // Remove `_id`
+    delete ret.__v;     // Remove `__v` (Mongoose version key)
+  }
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+
 
 module.exports = Product;
